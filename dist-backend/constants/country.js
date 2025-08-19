0d15697de70037cb35eae4fb43d0d61faa6e18ed
Stevy91 <stevesainthubert701@gmail.com>
2025-08-19 17:09:12 -0400

    Description courte

--- dist-backend/constants/country.js
+++
@@ -1,7 +0,0 @@
-export const COUNTRIES = [
-    { code: 'ht', name: 'Haiti' },
-    { code: 'fr', name: 'France' },
-    { code: 'us', name: 'États-Unis' },
-    { code: 'gb', name: 'Royaume-Uni' },
-    // Ajoutez tous les pays nécessaires
-];
